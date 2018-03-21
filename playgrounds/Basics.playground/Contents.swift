ae4eea82d2e9e7ec8984253f989a5a8affe28d22
Anthony Lee <antwonlee@gmail.com>
2018-03-20 23:02:17 -0500

    Add concatenating

---
+++ playgrounds/Basics.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var todo: [String] = ["A", "B", "C"]
+
+todo.append("Hello")
+
+// concanating
+
+[1,2,3] + [4,5,6]
+
+todo + ["What", "This", "Is", "Crazy!"]
